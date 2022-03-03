5b4d2489cf56a076a1c0c215444d87e1108613b4
Daniel James Dolor <estheryahe@gmail.com>
2022-03-03 16:12:16 +0800

    Initial Commit

---
+++ Robot/Robot.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class Robot : Spatial
+{
+    [Export]
+    private string program = "";
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
