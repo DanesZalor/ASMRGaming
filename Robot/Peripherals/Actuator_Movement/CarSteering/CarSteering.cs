1f5078117c846b6d6795884999766477a73e5fed
Daniel James Dolor <estheryahe@gmail.com>
2022-03-05 20:18:23 +0800

    implemented logic of car steering

--- Robot/Peripherals/Actuator_Movement/CarSteering/CarSteering.cs
+++ Robot/Peripherals/Actuator_Movement/CarSteering/CarSteering.cs
@@ -11,13 +11,20 @@ public class CarSteering : Peripheral
     public override void Init()
     {
         base.Init();
-        writeToRam(0, 0);
+        writeToRam(0, 127);
         writeToRam(1, 127);
     }
 
     public override void tickLogical(float delta)
     {
         //throw new NotImplementedException();
+        float accel = (((float)readFromRam(1) - 127f) / 127f) * delta * 45f;
+        float steering = (((float)readFromRam(0) - 127f) / 127f)  ;
+
+        parent.RotationDegrees -= Vector3.Up * steering * delta * 135f * accel;
+
+        Vector3 vel = GlobalTransform.basis.z * delta * 15f * accel;
+        parent.MoveAndCollide(vel);
     }
 
     public override void tickPresentational(float delta)
