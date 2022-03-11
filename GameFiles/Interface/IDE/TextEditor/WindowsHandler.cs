c71c030884dc778d9a87f31d5c8782c8cb172e3d
Daniel James Dolor <estheryahe@gmail.com>
2022-03-11 23:00:13 +0800

    cleaned the code. I got screwed up by earlier optimization. never again

--- GameFiles/Interface/IDE/TextEditor/WindowsHandler.cs
+++ GameFiles/Interface/IDE/TextEditor/WindowsHandler.cs
@@ -20,7 +20,8 @@ public class WindowsHandler : Node
     }
 
     private void delete_KeyFile(string filename){ // ASSUMING filename exist (it is in IDE.SaveFile.DATA) and IDE.SaveFile.Load()ed
-        IDE.SaveFile.DATA.Remove(filename);
+        if(filename.Equals("*")) IDE.SaveFile.DATA.Clear();
+        else IDE.SaveFile.DATA.Remove(filename);
         IDE.SaveFile.Save();
     }
 
@@ -39,76 +40,84 @@ public class WindowsHandler : Node
     private string rmCommand(string[] args){
         string s = "";
         for(int i = 1; i < args.Length; i++){
-            if(!keyFileExists(args[i])) 
-                s += String.Format("rm: cannot remove \'{0}\': No such file\n", args[i]); // BUGGY does not show for more than one line
+            if( !keyFileExists(args[i]) && !args[i].Equals("*") ) 
+                s += String.Format("rm: cannot remove \'{0}\': No such file\n", args[i]); 
             else
                 delete_KeyFile(args[i]);   
         }
         return s;
     }
+    private string touchCommand(string[] args){
+        string s = "";
+        for(int i = 1; i<args.Length; i++){
+            if(keyFileExists(args[i]))
+                s+= String.Format("touch: cannot create \'{0}\': file already exists\n", args[i]);
+            else{
+                if(args[i].Equals("*")) return "touch: cannot create \'*\'";
+                else create_KeyFile(args[i]);
+            }
+        }
+        return s;
+    }
 
-    private string openTextEditor(string filename){
+    private string editCommand(string[] args){
+        string s = "";
+        for(int i=1; i< args.Length; i++){
+            
+            if(!keyFileExists(args[i]))
+                s += String.Format("edit: \'{0}\': created since no such file existed\n",args[i]);
+            openTextEditor(args[i]);
+        }
+        return s;
+    }
+
+    private string catCommand(string[] args){
+        string s = "[color=#c0c0c0]";
+        for(int i=1; i<args.Length; i++){
+            if(keyFileExists(args[i])){
+                string content = IDE.SaveFile.DATA[args[i]] as String;
+                if(!content.EndsWith("\n")) content += "\n";
+                s += content;
+            }
+            else s+= String.Format("cat: \'{0}\': No such file\n", args[i]);
+        }
+        return s + "[/color]";
+    }
+
+    private void openTextEditor(string filename){
         ASMRTextEditor te = TEXT_EDITOR_PRELOAD.Instance<ASMRTextEditor>();
         te.setFileName(filename);
         AddChild(te);
-        return "";
     }
 
     public string interpretCommand(string[] args){
         IDE.SaveFile.Load();
-        if(args.Length>=1){
-            
-            if(args[0].Equals("ls")){
+   
+        if(args[0].Equals("ls")){
 
-                if(args.Length>1) return "[b]ls[/b] requires no arguements";
+            if(args.Length>1) return "[b]ls[/b] requires no arguements";
 
-                string r = "";
-                foreach(string s in IDE.SaveFile.DATA.Keys)
-                    r += String.Format("[color=#c0c0c0]{0}[/color]\t", s);
-                return r;
-            }
-
-            //if(args.Length<2) return String.Format("{0} : requires an arguement", args[0]);
-
-            else if( Global.match(args[0], "(touch|cat|edit|rm)") ){
-                
-                if(args.Length<2) return "[b]"+args[0]+"[/b] requires at least 1 filename arguement";
-
-                if( keyFileExists(args[1]) ){ // keyfile exists
-                    
-                    if( args[0].Equals("touch") ) 
-                        return "touch : \""+args[1]+"\" exists";
-                    
-                    else{ // args[0] == (cat|edit|rm)
-                        
-                        if( args[0].Equals("cat") ) 
-                            return (IDE.SaveFile.DATA[args[1]] as String);
-                        
-                        else if(args[0].Equals("edit"))
-                            return openTextEditor(args[1]);
-
-                        else if(args[0].Equals("rm"))
-                            return rmCommand(args);
-                    }
-                }
-                else{ // keyfile does not exist
-
-                    if( args[0].Equals("touch") )
-                        return create_KeyFile(args[1]);
-                    
-                    else // args[0] == cat|edit|rm
-                        return String.Format("[b]{0}[/b] : {1} : no such file", args[0], args[1]);
-
-                }
-            }
-            else if( args[0].Equals("mv")){
-
-                if(args.Length!=3) return "mv : requires 2 filename arguements";
-                else return rename_KeyFile(args[1], args[2]);
-
-            }
-
+            string r = "";
+            foreach(string s in IDE.SaveFile.DATA.Keys)
+                r += String.Format("[color=#c0c0c0]{0}[/color]\t", s);
+            return r;
         }
+        
+        if(Global.match(args[0],"(touch|rm|edit|cat)") && args.Length<2) 
+            return String.Format("{0}: needs atleast one filename arguement", args[0]);
+
+        if(args[0].Equals("rm"))
+            return rmCommand(args);
+        
+        else if(args[0].Equals("touch"))
+            return touchCommand(args);
+        
+        else if(args[0].Equals("edit"))
+            return editCommand(args);
+
+        else if(args[0].Equals("cat"))
+            return catCommand(args);
+        
         return "";
     }
 
