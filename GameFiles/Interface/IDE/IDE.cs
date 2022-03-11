3cc4b3bd8e92e424748e12ee133bee453c8d96c9
Daniel James Dolor <estheryahe@gmail.com>
2022-03-11 11:48:01 +0800

    added SaveFile class

--- GameFiles/Interface/IDE/IDE.cs
+++ GameFiles/Interface/IDE/IDE.cs
@@ -1,9 +1,42 @@
 using Godot;
 using System;
 
+
 public class IDE : Node
 {
-    
+    public static class SaveFile{
+        public const string path = "user://saveFile.json";
+        private static Godot.File file = new Godot.File();
+        
+        /// <summary> read file as string </summary>
+        public static string Load(){
+            string r = "";
+            if(file.FileExists(path)){
+                file.Open(path, Godot.File.ModeFlags.Read);
+                r = file.GetAsText();
+            
+            }else
+                file.Open(path, Godot.File.ModeFlags.Write);
+
+            file.Close();
+            return r;
+        }
+
+        public static Godot.Collections.Dictionary LoadDict(){
+            Godot.JSONParseResult jparse = Godot.JSON.Parse(Load());
+            return jparse.Result as Godot.Collections.Dictionary;
+        }
+
+        public static void Save(string content){
+            file.Open(path, Godot.File.ModeFlags.Write);
+            file.StoreString(content);
+            file.Close();
+        }
+
+        public static void Save(Godot.Collections.Dictionary<string,string> dict){
+            Save(JSON.Print(dict));
+        }
+    }
     private InterfaceConsole console;
     public RobotsHolder robots;
     private Spatial camHolder; private Camera cam;
