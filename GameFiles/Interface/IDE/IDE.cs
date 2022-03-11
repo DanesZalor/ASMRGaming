1cef0652e4f10382359ad1b27df21d9d4f593ac0
Daniel James Dolor <estheryahe@gmail.com>
2022-03-11 13:03:44 +0800

    added cameraprocess to dynamically move

--- GameFiles/Interface/IDE/IDE.cs
+++ GameFiles/Interface/IDE/IDE.cs
@@ -60,17 +60,35 @@ public class IDE : Node
 
         Vector2 ave = Vector2.Zero;
         if(robots.GetChildCount()>=1){
+            
+            Vector2 min = new Vector2(int.MaxValue, int.MaxValue); 
+            Vector2 max = new Vector2(int.MinValue, int.MinValue);
+
             foreach(Spatial b in robots.GetChildren()){
                 ave.x += b.GlobalTransform.origin.x;
                 ave.y += b.GlobalTransform.origin.z;
+
+                min = new Vector2(
+                    Mathf.Min(min.x, b.GlobalTransform.origin.x),
+                    Mathf.Min(min.y, b.GlobalTransform.origin.z)
+                );
+                max = new Vector2(
+                    Mathf.Max(max.x, b.GlobalTransform.origin.x),
+                    Mathf.Max(max.y, b.GlobalTransform.origin.z)
+                );
             }
+
+            float maxdiff = Mathf.Max(
+                Mathf.Max( max.x-min.x, (max.y-min.y)*1.2f )
+            ,40);
+            cam.Size = Mathf.Lerp(cam.Size, 30 * (maxdiff/40f), 0.1f);
             
             ave /= robots.GetChildCount();
         } 
 
         camHolder.Translation = new Vector3(
-            Mathf.Lerp(camHolder.Translation.x, ave.x, 0.2f), 0,
-            Mathf.Lerp(camHolder.Translation.z, ave.y, 0.2f)
+            Mathf.Lerp(camHolder.Translation.x, ave.x, 0.1f), 0,
+            Mathf.Lerp(camHolder.Translation.z, ave.y, 0.1f)
         );
     } 
 
