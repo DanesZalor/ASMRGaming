d519bfd42dd08a6a1d643f99647d394e7633b017
Daniel James Dolor <estheryahe@gmail.com>
2022-03-11 11:56:19 +0800

    cleaned up SaveFile class

--- GameFiles/Interface/IDE/IDE.cs
+++ GameFiles/Interface/IDE/IDE.cs
@@ -7,9 +7,10 @@ public class IDE : Node
     public static class SaveFile{
         public const string path = "user://saveFile.json";
         private static Godot.File file = new Godot.File();
-        
-        /// <summary> read file as string </summary>
-        public static string Load(){
+        private static Godot.Collections.Dictionary data; 
+        public static Godot.Collections.Dictionary DATA { get => data; }
+
+        private static string Read(){
             string r = "";
             if(file.FileExists(path)){
                 file.Open(path, Godot.File.ModeFlags.Read);
@@ -21,20 +22,17 @@ public class IDE : Node
             file.Close();
             return r;
         }
-
-        public static Godot.Collections.Dictionary LoadDict(){
-            Godot.JSONParseResult jparse = Godot.JSON.Parse(Load());
-            return jparse.Result as Godot.Collections.Dictionary;
-        }
-
-        public static void Save(string content){
+        private static void Write(string content){
             file.Open(path, Godot.File.ModeFlags.Write);
             file.StoreString(content);
             file.Close();
         }
-
-        public static void Save(Godot.Collections.Dictionary<string,string> dict){
-            Save(JSON.Print(dict));
+        public static void Load(){
+            Godot.JSONParseResult jparse = Godot.JSON.Parse(Read());
+            data = jparse.Result as Godot.Collections.Dictionary;
+        }
+        public static void Save(){
+            Write(JSON.Print(data));
         }
     }
     private InterfaceConsole console;
@@ -49,6 +47,7 @@ public class IDE : Node
 
         robots = GetNode<RobotsHolder>("Robots");
         console = GetNode<InterfaceConsole>("Console");
+
     }
 
 }
