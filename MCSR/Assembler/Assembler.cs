6edd6ffe34ca00694770379576180f33f552abf7
Daniel James Dolor <estheryahe@gmail.com>
2022-03-12 07:50:50 +0800

    added way to bypass syntax checking for game purposes

--- MCSR/Assembler/Assembler.cs
+++ MCSR/Assembler/Assembler.cs
@@ -2,18 +2,21 @@
 
 public static class Assembler{
 
-    public static byte[] compile(string linesOfCode){
+    public static byte[] compile(string linesOfCode, bool checkSyntax=true){
         
         byte[] r = new byte[0];
         
         // check syntax first
-        string syntaxErrors = SyntaxChecker.evaluateProgram(linesOfCode);
-        if(syntaxErrors != "") return r;
+        if(checkSyntax){
+            string syntaxErrors = SyntaxChecker.evaluateProgram(linesOfCode);
+            if(syntaxErrors != "") return r;
+        }
 
         string derivedVer = PreprocessorDirectives.translateAlias(linesOfCode);
 
         return Translator.translateProgram(derivedVer);
     }
+
 }
 
 }
