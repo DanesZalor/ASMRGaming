21fa4af691a2cbe1f41eb99ca68c7a9429308e9c
Daniel James Dolor <estheryahe@gmail.com>
2022-03-20 21:19:29 +0800

    adding startmenu cli/bios esque aesthetic

---
+++ GameFiles/Interface/StartCLI/StartCLI.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class StartCLI : Control
+{
+
+    public override void _Ready()
+    {
+        //Engine.TargetFps = 30; 
+        //OS.VsyncEnabled = false; 
+        //OS.WindowFullscreen = true; 
+    }
+}
