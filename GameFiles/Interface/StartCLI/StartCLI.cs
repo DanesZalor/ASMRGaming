40cda11199ee284275733206301837651e07feea
Daniel James Dolor <estheryahe@gmail.com>
2022-03-21 13:00:36 +0800

    added menu switching

--- GameFiles/Interface/StartCLI/StartCLI.cs
+++ GameFiles/Interface/StartCLI/StartCLI.cs
@@ -3,11 +3,26 @@ using System;
 
 public class StartCLI : Control
 {
-
+    private Control[] menuContent;
     public override void _Ready()
     {
+        menuContent = new Control[3]{
+            GetNode<Control>("Menu/Content/Game"),
+            GetNode<Control>("Menu/Content/Settings"),
+            GetNode<Control>("Menu/Content/About")
+        };
+
         //Engine.TargetFps = 30; 
         //OS.VsyncEnabled = false; 
         //OS.WindowFullscreen = true; 
+
+    }
+
+    public void headerPressed(int idx){
+        GD.Print("header press");
+        GD.Print(idx);
+        for(int i=0; i<menuContent.Length; i++)
+            menuContent[i].Visible = (i==idx);
+        
     }
 }
