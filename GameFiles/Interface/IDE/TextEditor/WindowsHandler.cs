1cb6558c0dfa7dfaf0b6c845e0958c2949dcbca6
Daniel James Dolor <estheryahe@gmail.com>
2022-03-22 13:51:34 +0800

    deleted old StartScreen

--- GameFiles/Interface/IDE/TextEditor/WindowsHandler.cs
+++ GameFiles/Interface/IDE/TextEditor/WindowsHandler.cs
@@ -3,7 +3,6 @@ using System;
 
 public class WindowsHandler : Control
 {
-    public static PackedScene TEXT_EDITOR_PRELOAD = GD.Load<PackedScene>("res://GameFiles/Interface/IDE/TextEditor/TextEditor.tscn");
     public static PackedScene WINDOW = GD.Load<PackedScene>("res://GameFiles/Interface/Window/WindowContainer.tscn");
     public static PackedScene NOTEPAD = GD.Load<PackedScene>("res://GameFiles/Interface/Window/Notepad/Notepad.tscn");
     public static PackedScene COMMAND_PROMPT = GD.Load<PackedScene>("res://GameFiles/Interface/Window/CommandPrompt/CommandPrompt.tscn");
